1b7acee91f511d11613112f60efc11e683115339
eugenia <eugenius31415@gmail.com>
2026-07-31 11:32:30 -0700

    headers

---
+++ UI/ChargeLED.h
@@ -0,0 +1,8 @@
+#ifndef CHARGELED_H
+#define CHARGELED_H
+
+void ChargeLED_init();
+
+void ChargeLED_update(float batteryPercent, bool usbConnected);
+
+#endif
---
+++ UI/PowerLED.h
@@ -0,0 +1,9 @@
+#ifndef POWERLED_H
+#define POWERLED_H
+
+void LED_init();
+void LED_sleep();
+void LED_awake();
+void LED_off();
+
+#endif
