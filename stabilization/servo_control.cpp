3e259ac17194b40e21fc334cdcc7ca4d44d01943
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-07-31 12:35:33 -0700

    Add files via upload

---
+++ stabilization/servo_control.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include "servo_control.h"
+#include "configuration.h"
+
+Servo wristPitchServo;
+//Servo wristYawServo; final goal: have three servos
+//Servo elbowServo;
+
+void setupServos()
+{
+    wristPitchServo.attach(WRIST_PITCH_PIN);
+    //wristYawServo.attach(WRIST_SIDE_PIN);
+    //elbowServo.attach(ELBOW_PIN);
+
+    // Start all servos at their centre positions
+    wristPitchServo.write(WRIST_PITCH_START);
+    //wristSideServo.write(WRIST_YAW_START);
+    //elbowServo.write(ELBOW_START);
+}
+
+void moveWristPitch(int angle)
+{
+    angle = constrain(angle, WRIST_PITCH_MIN, WRIST_PITCH_MAX);
+    wristPitchServo.write(angle);
+}
+
+/*
+void moveWristYaw(int angle)
+{
+    angle = constrain(angle, WRIST_YAW_MIN, WRIST_YAW_MAX);
+    wristSideServo.write(angle);
+}
+
+void moveElbow(int angle)
+{
+    angle = constrain(angle, ELBOW_MIN, ELBOW_MAX);
+    elbowServo.write(angle);
+}
+*/
---
+++ stabilization/servo_control.h
@@ -0,0 +1,4 @@
+// this file
+
+void setupServos();
+void moveWristPitch();
