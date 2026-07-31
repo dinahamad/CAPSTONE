e6fc51e04947b1da6e660ef969436dd4ad1f2dc5
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-07-31 12:51:44 -0700

    Update servo_control.cpp

--- stabilization/servo_control.cpp
+++ stabilization/servo_control.cpp
@@ -1,7 +1,25 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include "servo_control.h"
-#include "configuration.h"
+
+// --------- SERVO CONSTANTS
+// Servo pins
+const int WRIST_PITCH_PIN = 14; // up and down
+//const int WRIST_YAW_PIN = #; // side to side
+//const int ELBOW_PIN = #;
+
+// Servo starting positions, based on gemoetry of arm
+const int WRIST_PITCH_START = 90;
+//const int WRIST_YAW_START  = 90;
+//const int ELBOW_START       = 90;
+
+// Servo soft limits
+const int WRIST_PITCH_MIN = 40;
+const int WRIST_PITCH_MAX = 140;
+//const int WRIST_YAW_MIN = #;
+//const int WRIST_YAW_MAX = #;
+//const int ELBOW_MIN = #;
+//const int ELBOW_MAX = #;
 
 Servo wristPitchServo;
 //Servo wristYawServo; final goal: have three servos
