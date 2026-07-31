8037a249d06d79fad426a511594d3cfaa54fa222
eugenia <eugenius31415@gmail.com>
2026-07-31 11:28:19 -0700

    Create ChargeLED.cpp

---
+++ UI/ChargeLED.cpp
@@ -0,0 +1,94 @@
+#include <Arduino.h>
+#include "ChargeLED.h"
+
+#define CHARGE_LED1 12
+#define CHARGE_LED2 27
+#define CHARGE_LED3 33
+
+
+unsigned long lastBlink = 0;
+bool blinkState = false;
+
+
+void ChargeLED_init() {
+
+    pinMode(CHARGE_LED1, OUTPUT);
+    pinMode(CHARGE_LED2, OUTPUT);
+    pinMode(CHARGE_LED3, OUTPUT);
+
+    digitalWrite(CHARGE_LED1, LOW);
+    digitalWrite(CHARGE_LED2, LOW);
+    digitalWrite(CHARGE_LED3, LOW);
+}
+
+
+void setLEDs(bool led1, bool led2, bool led3) {
+
+    digitalWrite(CHARGE_LED1, led1);
+    digitalWrite(CHARGE_LED2, led2);
+    digitalWrite(CHARGE_LED3, led3);
+}
+
+
+void ChargeLED_update(float batteryPercent, bool usbConnected) {
+
+    // Blink every 500 ms
+    if (millis() - lastBlink > 500) {
+        lastBlink = millis();
+        blinkState = !blinkState;
+    }
+
+
+    // ===========================
+    // NOT CHARGING (battery mode)
+    // ===========================
+    if (!usbConnected) {
+
+        if (batteryPercent < 20) {
+            // LED1 flashing
+            setLEDs(blinkState, false, false);
+        }
+
+        else if (batteryPercent < 40) {
+            // LED1 solid
+            setLEDs(true, false, false);
+        }
+
+        else if (batteryPercent < 70) {
+            // LED1 + LED2
+            setLEDs(true, true, false);
+        }
+
+        else {
+            // LED1 + LED2 + LED3
+            setLEDs(true, true, true);
+        }
+    }
+
+
+    // ===========================
+    // CHARGING (USB connected)
+    // ===========================
+    else {
+
+        if (batteryPercent < 33) {
+            // LED1 flashing
+            setLEDs(blinkState, false, false);
+        }
+
+        else if (batteryPercent < 66) {
+            // LED1 solid, LED2 flashing
+            setLEDs(true, blinkState, false);
+        }
+
+        else if (batteryPercent < 100) {
+            // LED1 + LED2 solid, LED3 flashing
+            setLEDs(true, true, blinkState);
+        }
+
+        else {
+            // Full
+            setLEDs(true, true, true);
+        }
+    }
+}
