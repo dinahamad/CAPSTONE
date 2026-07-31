a693c50e49296ca8dae1688471cb8f73748c69d7
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-07-31 13:40:04 -0700

    Update servo_control.h

--- stabilization/servo_control.h
+++ stabilization/servo_control.h
@@ -2,22 +2,17 @@
 
 // --------- SERVO CONSTANTS
 // Servo pins
-const int WRIST_PITCH_PIN = 14; // up and down
-//const int WRIST_YAW_PIN = #; // side to side
-//const int ELBOW_PIN = #;
+const int WRIST_PITCH_TOP_PIN = 14; 
+const int WRIST_PITCH_BOTTOM_PIN = 15;
 
 // Servo starting positions, based on gemoetry of arm
-const int WRIST_PITCH_START = 90;
-//const int WRIST_YAW_START  = 90;
-//const int ELBOW_START       = 90;
+const int WRIST_PITCH_TOP_START = 90;
+const int WRIST_PITCH_BOTTOM_START = 90;
 
 // Servo soft limits
 const int WRIST_PITCH_MIN = 40;
 const int WRIST_PITCH_MAX = 140;
-//const int WRIST_YAW_MIN = #;
-//const int WRIST_YAW_MAX = #;
-//const int ELBOW_MIN = #;
-//const int ELBOW_MAX = #;
 
 void setupServos();
-void moveWristPitch();
+void moveWristTopPitch(int angle);
+void moveWristBottomPitch(int angle);
