dcfef9e73bb74401cfe1c6458de606c44b544ce0
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-08-02 11:47:37 -0700

    Update servo_control.cpp

--- stabilization/servo_control.cpp
+++ stabilization/servo_control.cpp
@@ -1,70 +1,108 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include "servo_control.h"
+#include <iostream>
 
-// servo objects
-Servo wristPitchTopServo;
-Servo wristPitchBottomServo;
+/////////////////////////////////////////////////////////////////////////////////////////////
+// This file is for the servos that pull the hand up and down relative to the forearm (pitch)
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-// Current servo angles. Start at neutral
-static int wristPitchTopServoCommand = WRIST_PITCH_TOP_START;
-static int wristPitchBottomServoCommand =WRIST_PITCH_BOTTOM_START;
+// Servo objects
+Servo topServo;
+Servo bottomServo;
+
+// start at neutral, changes for evreythime it moves
+int TopServoCurrentAngle = TOP_SERVO_ORIGIN_ANGLE;
+int BottomServoCurrentAngle = BOTTOM_SERVO_ORIGIN_ANGLE;
 
 // Whether the servos are currently attached
-static bool servosEnabled = false;
+bool servosEnabled = false;
 
-void setupServos()
-{
+// connect servos to microcontroller
+void setupServos() {
     // Connect each servo object to its pin
-    wristPitchTopServo.attach(WRIST_PITCH_TOP_PIN);
-    wristPitchBottomServo.attach(WRIST_PITCH_BOTTOM_PIN);
+    topServo.attach(TOP_SERVO_PIN);
+    bottomServo.attach(BOTTOM_SERVO_PIN);
     servosEnabled = true; // they are attached
     // Put both servos at their starting positions
-    stopWrist();
+    originAngles();
 }
 
-// Main function called by the control algorithm
-void setWristCorrection(float correction) {
-    if (correction > 0.0f) {moveHandUp((int)correction);}
-    else if (correction < 0.0f) {moveHandDown((int)(-correction));}
-    else{stopWrist();}
+// FUNCTIONS FOR VOLUNTARY MOVEMENT AKA WHERE THE "CENTER" OF THE HAND IS///////////////////////////////////////////////////////////
+void setWristPitchValue(float correction) {
+    if (correction > 0) {moveHandUp((int)correction);}
+    else if (correction < 0) {moveHandDown((int)(-correction));}
 }
 
-
+// both the directions need to be opposite for each command (-1 and 1 or vice versa)
+// later have one variable for up one for down
 void moveHandUp(int amount)
 {
-    // amount must be between 0 max pull amount
-    amount = constrain(amount, WRIST_PITCH_TOP_MIN, WRIST_PITCH_TOP_MAX);
+    // servos pulls upward with the hand
+    int topAngle = TopServoCurrentAngle + amount;
+    int bottomAngle = BottomServoCurrentAngle + amount;
 
-    // Top servo pulls upward
-    int topAngle = amount * WRIST_PITCH_TOP_DIRECTION;
+    // constrain to the safety limit for both servos
+    topAngle = constrain(topAngle, TOP_SERVO_MIN_ANGLE, TOP_SERVO_MAX_ANGLE);
+    bottomAngle = constrain(bottomAngle, BOTTOM_SERVO_MIN_ANGLE, BOTTOM_SERVO_MAX_ANGLE);
 
-    // Top servo pulls; bottom servo returns to neutral
-    wristPitchTopServo.write(topAngle);
-    wristPitchBottomServo.write(WRIST_PITCH_BOTTOM_START);
+    topServo.write(topAngle);
+    bottomServo.write(bottomAngle);
 
-    wristPitchTopServoCommand = topAngle;
+    // update current angles
+    TopServoCurrentAngle = topAngle;
+    BottomServoCurrentAngle = bottomAngle;
 }
 
-void moveHandDown(int amount)
-{
-    amount = constrain(amount, WRIST_PITCH_BOTTOM_MIN, WRIST_PITCH_BOTTOM_MAX);
+void moveHandDown(int amount) {
+    // servo pulls downwards with the hand
+    int topAngle = TopServoCurrentAngle - amount;
+    int bottomAngle = BottomServoCurrentAngle - amount;
 
-    // Bottom servo pulls downward
-    int bottomAngle = amount * WRIST_PITCH_BOTTOM_DIRECTION;
+    // constrain to the safety limit for both servos
+    topAngle = constrain(topAngle, TOP_SERVO_MIN_ANGLE, TOP_SERVO_MAX_ANGLE);
+    bottomAngle = constrain(bottomAngle, BOTTOM_SERVO_MIN_ANGLE, BOTTOM_SERVO_MAX_ANGLE);
 
-    // Bottom servo pulls; top servo returns to neutral
-    wristPitchTopServo.write(WRIST_PITCH_TOP_START);
-    wristPitchBottomServo.write(bottomAngle);
+    topServo.write(topAngle);
+    bottomServo.write(bottomAngle);
 
-    wristPitchBottomServoCommand = bottomAngle;
+    // update current angles
+    TopServoCurrentAngle = topAngle;
+    BottomServoCurrentAngle = bottomAngle;
 }
 
+// FUNCTIONS FOR TREMOR MOVEMENT/////////////////////////////////////////////////////////////////////////////////////////////
 
-void stopWrist()
-{
+
+
+
+
+
+
+void originAngles() {
     // Neither servo actively corrects the wrist, put back to neutral
-    wristPitchTopServo.write(WRIST_PITCH_TOP_START);
-    wristPitchBottomServo.write(WRIST_PITCH_BOTTOM_START);
+    topServo.write(TOP_SERVO_ORIGIN_ANGLE);
+    bottomServo.write(BOTTOM_SERVO_ORIGIN_ANGLE);
+
+    // update current angles
+    TopServoCurrentAngle = TOP_SERVO_ORIGIN_ANGLE;
+    BottomServoCurrentAngle = BOTTOM_SERVO_ORIGIN_ANGLE;   
 }
 
+void endProgram() {
+  topServo.detach();
+  bottomServo.detach();
+  servosEnabled = false;
+}
+
+int getTopServoAngle() {
+    return(TopServoCurrentAngle);
+};
+
+int getBottomServoAngle() {
+    return(BottomServoCurrentAngle);
+};
+
+bool areServosEnabled(){
+    return servosEnabled;
+};
