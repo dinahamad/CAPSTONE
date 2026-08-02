cca8ffa0caad0dbc9798a98983ee5003868a502a
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-08-01 18:38:34 -0700

    Update servo_control.cpp

--- stabilization/servo_control.cpp
+++ stabilization/servo_control.cpp
@@ -6,15 +6,30 @@
 Servo wristPitchTopServo;
 Servo wristPitchBottomServo;
 
+// Current servo angles. Start at neutral
+static int wristPitchTopServoCommand = WRIST_PITCH_TOP_START;
+static int wristPitchBottomServoCommand =WRIST_PITCH_BOTTOM_START;
+
+// Whether the servos are currently attached
+static bool servosEnabled = false;
+
 void setupServos()
 {
     // Connect each servo object to its pin
     wristPitchTopServo.attach(WRIST_PITCH_TOP_PIN);
     wristPitchBottomServo.attach(WRIST_PITCH_BOTTOM_PIN);
-
+    servosEnabled = true; // they are attached
     // Put both servos at their starting positions
     stopWrist();
 }
+
+// Main function called by the control algorithm
+void setWristCorrection(float correction) {
+    if (correction > 0.0f) {moveHandUp((int)correction);}
+    else if (correction < 0.0f) {moveHandDown((int)(-correction));}
+    else{stopWrist();}
+}
+
 
 void moveHandUp(int amount)
 {
@@ -22,11 +37,13 @@ void moveHandUp(int amount)
     amount = constrain(amount, WRIST_PITCH_TOP_MIN, WRIST_PITCH_TOP_MAX);
 
     // Top servo pulls upward
-    int topAngle = WRIST_PITCH_TOP_START + amount * WRIST_PITCH_TOP_DIRECTION;
+    int topAngle = amount * WRIST_PITCH_TOP_DIRECTION;
 
     // Top servo pulls; bottom servo returns to neutral
     wristPitchTopServo.write(topAngle);
     wristPitchBottomServo.write(WRIST_PITCH_BOTTOM_START);
+
+    wristPitchTopServoCommand = topAngle;
 }
 
 void moveHandDown(int amount)
@@ -34,11 +51,13 @@ void moveHandDown(int amount)
     amount = constrain(amount, WRIST_PITCH_BOTTOM_MIN, WRIST_PITCH_BOTTOM_MAX);
 
     // Bottom servo pulls downward
-    int bottomAngle = WRIST_PITCH_BOTTOM_START + amount * WRIST_PITCH_BOTTOM_DIRECTION;
+    int bottomAngle = amount * WRIST_PITCH_BOTTOM_DIRECTION;
 
     // Bottom servo pulls; top servo returns to neutral
     wristPitchTopServo.write(WRIST_PITCH_TOP_START);
     wristPitchBottomServo.write(bottomAngle);
+
+    wristPitchBottomServoCommand = bottomAngle;
 }
 
 
