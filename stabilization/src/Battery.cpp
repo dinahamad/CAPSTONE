d9dd11f5e37bbe9f89bfb85c3f6e12347cc87e4c
eugenia <eugenius31415@gmail.com>
2026-08-03 23:46:14 -0700

    UI related - need to update main to include

---
+++ stabilization/src/Battery.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include "Battery.h"
+#include "Pins.h"
+
+
+void Battery_init() {
+    pinMode(VBAT_PIN, INPUT);
+}
+
+
+float Battery_getVoltage() {
+
+    float vbat_mv = analogReadMilliVolts(VBAT_PIN);
+
+    // Feather V2 has a 200k/200k divider
+    return (vbat_mv * 2.0) / 1000.0;
+}
+
+
+float Battery_getPercentage() {
+
+    float voltage = Battery_getVoltage();
+
+    float percentage = ((voltage - 3.2) / (4.2 - 3.2)) * 100.0;
+
+    if (percentage > 100.0) percentage = 100.0;
+    if (percentage < 0.0) percentage = 0.0;
+
+    return percentage;
+}
+
+
+void Battery_printStatus() {
+
+    Serial.print("Battery Voltage: ");
+    Serial.print(Battery_getVoltage(), 2);
+    Serial.print(" V | ");
+
+    Serial.print("Approx. Charge: ");
+    Serial.print(Battery_getPercentage(), 1);
+    Serial.println("%");
+}
