701a1c583eee62a700e0b3fe681a56446b59beb1
dinahamad <147458415+dinahamad@users.noreply.github.com>
2026-08-05 13:59:03 -0700

    Update servo_math.h

--- stabilization/include/servo_math.h
+++ stabilization/include/servo_math.h
@@ -1,16 +1,31 @@
 #include "globals.h"
+#include "servo_control.h"
 
-const int HAND_IMU = 0;
-const int FOREARM_IMU = 1;
+// useful variables from main
+// float getRelativeOrientation{return relativeOrientation;} // should be a degree value btween 0 -> 180deg
 
-const int ROLL = 0;
-const int PITCH = 1;
-const int YAW = 2;
+// Troubleshooting
+bool isHandIMUConnected(){return imuConnected[0];}
+bool isForearmIMUConnected(){return imuConnected[1];}
+bool isTopServoConnected(){return topServoConnected;}
+bool isBottomServoConnected(){return bottomServoConnected;}
+bool isEverythingConnected(){return isHandIMUConnected() && isForearmIMUConnected() && isTopServoConnected() && isBottomServoConnected();}
+
+
+
+
+// ---------------------------- DELETE EVERYTHING PAST THIS LINE
+//const int HAND_IMU = 0;
+//const int FOREARM_IMU = 1;
+
+//const int ROLL = 0;
+//const int PITCH = 1;
+//const int YAW = 2;
 
 // of Hand IMU
-float rollAngleHand() {return orientation[HAND_IMU][ROLL];} // units: degrees
-float pitchAngleHand(){return orientation[HAND_IMU][PITCH];}
-float yawAngleHand(){return orientation[HAND_IMU][YAW];}
+// float rollAngleHand() {return orientation[HAND_IMU][ROLL];} // units: degrees
+// float pitchAngleHand(){return orientation[HAND_IMU][PITCH];}
+// float yawAngleHand(){return orientation[HAND_IMU][YAW];}
 
 // of forearm IMU
 float forearmRoll(){return orientation[FOREARM_IMU][ROLL];}
@@ -18,11 +33,10 @@ float forearmPitch(){return orientation[FOREARM_IMU][PITCH];}
 float forearmYaw(){return orientation[FOREARM_IMU][YAW];}
 
 // angle relative difference between hand and forearm - all basic uses this
-float wristRoll(){return relativeOrientation[ROLL];}
-float wristPitch(){return relativeOrientation[PITCH];}
-float wristYaw(){return relativeOrientation[YAW];}
+// float wristRoll(){return relativeOrientation[ROLL];}
+// float wristPitch(){return relativeOrientation[PITCH];}
+// float wristYaw(){return relativeOrientation[YAW];}
 
-// DNE
 // between hand and forearm
 // float gettremorFrequencyHz(){return tremorFrequencyHz;}
 // float batteryPercentage(); // disconnect servos once reaches a certain battery
