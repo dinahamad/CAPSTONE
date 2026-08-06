082f9421625f0c32151e0dcbe87addfe61bab4e3
eugenia <eugenius31415@gmail.com>
2026-08-06 12:37:34 -0700

    Add files via upload

---
+++ stabilization/include/UI.h
@@ -0,0 +1,13 @@
+#ifndef UI_H
+#define UI_H
+
+void USB_detect_init();
+bool USB_connected();
+
+void Button_waitForRelease();
+bool validButtonPress(uint8_t BUTTON_NUMBER);
+void Button_init();
+
+void changeStableState();
+
+#endif
